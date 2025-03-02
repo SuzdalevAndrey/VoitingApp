460794bca94cb54f085fd6baf484f708c44779da
Andrey <suzdalev2005@gmail.com>
2025-03-03 02:32:38 +0300

    Added logs for ServerConfiguration

--- server/src/main/java/ru/andreyszdlv/config/ServerConfiguration.java
+++ server/src/main/java/ru/andreyszdlv/config/ServerConfiguration.java
@@ -1,10 +1,12 @@
 package ru.andreyszdlv.config;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.InputStream;
 import java.util.Properties;
 
+@Slf4j
 @Getter
 public class ServerConfiguration {
 
@@ -12,15 +14,17 @@ public class ServerConfiguration {
 
     public ServerConfiguration(String propertiesFileName) {
         Properties properties = new Properties();
+        log.info("Loading configuration from file: {}", propertiesFileName);
         try (InputStream input = getClass().getClassLoader().getResourceAsStream(propertiesFileName)) {
             if (input == null) {
-                System.err.println("Файл конфигурации не найден");
+                log.error("Configuration file '{}' not found", propertiesFileName);
                 return;
             }
             properties.load(input);
             this.port = Integer.parseInt(properties.getProperty("server.port"));
+            log.info("Successfully loaded server configuration. Port: {}", this.port);
         } catch (Exception e) {
-            System.err.println("Ошибка при чтении файла конфигурации: " + e.getMessage());
+            log.error("Error reading configuration file \"{}\": {}", propertiesFileName, e.getMessage());
         }
     }
 }
