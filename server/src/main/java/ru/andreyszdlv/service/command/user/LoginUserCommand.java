f5d0a424cb00075e7b6738da2374edf80e9c923b
Andrey <suzdalev2005@gmail.com>
2025-03-02 15:09:40 +0300

    Refactor LoginUserCommand after added messages.properties

--- server/src/main/java/ru/andreyszdlv/service/command/user/LoginUserCommand.java
+++ server/src/main/java/ru/andreyszdlv/service/command/user/LoginUserCommand.java
@@ -3,6 +3,7 @@ package ru.andreyszdlv.service.command.user;
 import io.netty.channel.ChannelHandlerContext;
 import lombok.RequiredArgsConstructor;
 import ru.andreyszdlv.repo.UserRepository;
+import ru.andreyszdlv.util.MessageProviderUtil;
 import ru.andreyszdlv.util.ParameterUtils;
 
 @RequiredArgsConstructor
@@ -14,28 +15,28 @@ public class LoginUserCommand implements UserCommandHandler {
     public void execute(ChannelHandlerContext ctx, String[] paramsCommand) {
 
         if (paramsCommand.length != 1) {
-            ctx.writeAndFlush("Ошибка: неверная команда. Пример: login -u=user");
+            ctx.writeAndFlush(MessageProviderUtil.getMessage("error.command.login.invalid"));
             return;
         }
 
         String username = ParameterUtils.extractValueByPrefix(paramsCommand[0], "-u=");
 
         if(username == null) {
-            ctx.writeAndFlush("Ошибка: неверная команда. Пример: login -u=user");
+            ctx.writeAndFlush(MessageProviderUtil.getMessage("error.command.login.invalid"));
             return;
         }
 
         if(username.isBlank()){
-            ctx.writeAndFlush("Ошибка: пустое имя!");
+            ctx.writeAndFlush(MessageProviderUtil.getMessage("error.user.name.empty"));
             return;
         }
 
         if(userRepository.containsUserByName(username)) {
-            ctx.writeAndFlush(String.format("Ошибка: пользователь \"%s\" уже есть в системе!", username));
+            ctx.writeAndFlush(MessageProviderUtil.getMessage("error.user.already_exist", username));
             return;
         }
 
         userRepository.saveUser(ctx.channel().id().asLongText(), username);
-        ctx.writeAndFlush(String.format("Пользователь \"%s\" вошел в систему!", username));
+        ctx.writeAndFlush(MessageProviderUtil.getMessage("command.login.success", username));
     }
 }
