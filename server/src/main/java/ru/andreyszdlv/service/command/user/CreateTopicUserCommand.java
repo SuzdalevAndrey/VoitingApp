14d472ff36fcd013263bbee739d67d5f831f28e9
Andrey <suzdalev2005@gmail.com>
2025-03-02 15:27:17 +0300

    Refactor CreateTopicUserCommand after added messages.properties

--- server/src/main/java/ru/andreyszdlv/service/command/user/CreateTopicUserCommand.java
+++ server/src/main/java/ru/andreyszdlv/service/command/user/CreateTopicUserCommand.java
@@ -4,6 +4,7 @@ import io.netty.channel.ChannelHandlerContext;
 import lombok.RequiredArgsConstructor;
 import ru.andreyszdlv.model.Topic;
 import ru.andreyszdlv.repo.TopicRepository;
+import ru.andreyszdlv.util.MessageProviderUtil;
 import ru.andreyszdlv.util.ParameterUtils;
 
 @RequiredArgsConstructor
@@ -15,28 +16,32 @@ public class CreateTopicUserCommand implements UserCommandHandler {
     public void execute(ChannelHandlerContext ctx, String[] paramsCommand) {
 
         if(paramsCommand.length != 1) {
-            ctx.writeAndFlush("Ошибка: неверная команда. Пример: create topic -n=НазваниеТопика");
+            ctx.writeAndFlush(MessageProviderUtil
+                    .getMessage("error.command.create_topic.invalid"));
             return;
         }
 
         String topicName = ParameterUtils.extractValueByPrefix(paramsCommand[0], "-n=");
 
         if(topicName == null) {
-            ctx.writeAndFlush("Ошибка: неверная команда. Пример: create topic -n=НазваниеТопика");
+            ctx.writeAndFlush(MessageProviderUtil
+                    .getMessage("error.command.create_topic.invalid"));
             return;
         }
 
         if(topicName.isBlank()){
-            ctx.writeAndFlush("Ошибка: пустое имя!");
+            ctx.writeAndFlush(MessageProviderUtil.getMessage("error.topic.name.empty"));
             return;
         }
 
         if(topicRepository.containsTopicByName(topicName)) {
-            ctx.writeAndFlush(String.format("Ошибка: топик с именем \"%s\" уже существует!", topicName));
+            ctx.writeAndFlush(MessageProviderUtil
+                    .getMessage("error.topic.already_exist", topicName));
             return;
         }
 
         topicRepository.saveTopic(new Topic(topicName));
-        ctx.writeAndFlush(String.format("Топик с именем \"%s\" создан!", topicName));
+        ctx.writeAndFlush(MessageProviderUtil
+                .getMessage("command.create_topic.success", topicName));
     }
 }
