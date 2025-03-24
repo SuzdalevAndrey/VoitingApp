bce1b0285cb2cd359766a9b37d43a0aed79e691f
SuzdalevAndrey <suzdalev2005@gmail.com>
2025-03-24 14:35:46 +0300

    Fix test for ViewUserCommandTest

--- server/src/test/java/ru/andreyszdlv/service/command/user/ViewUserCommandTest.java
+++ server/src/test/java/ru/andreyszdlv/service/command/user/ViewUserCommandTest.java
@@ -9,7 +9,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import ru.andreyszdlv.model.Topic;
 import ru.andreyszdlv.model.Vote;
 import ru.andreyszdlv.repo.TopicRepository;
-import ru.andreyszdlv.util.MessageProviderUtil;
+import ru.andreyszdlv.service.MessageService;
 
 import java.util.Map;
 import java.util.Optional;
@@ -23,169 +23,138 @@ class ViewUserCommandTest {
     TopicRepository topicRepository;
 
     @Mock
+    MessageService messageService;
+
+    @Mock
     ChannelHandlerContext ctx;
 
     @InjectMocks
     ViewUserCommand viewUserCommand;
 
     @Test
-    void execute_ShowAllTopics_WhenNoParamsAndTopicExists() {
+    void execute_SendAllTopics_WhenNoParamsAndTopicExists() {
         String[] param = new String[]{};
         Map<String, Topic> topics = Map.of(
                 "Topic1", new Topic("Topic1"),
                 "Topic2", new Topic("Topic2")
         );
+        StringBuilder response = new StringBuilder();
+        for (String key : topics.keySet()) {
+            response.append(String.format("\"%s\" (votes in topic=0)\n", key));
+        }
+        response.setLength(response.length() - System.lineSeparator().length());
+
         when(topicRepository.findAll()).thenReturn(topics);
 
         viewUserCommand.execute(ctx, param);
 
-        for (Map.Entry<String, Topic> entry : topics.entrySet()) {
-            verify(ctx, times(1))
-                    .writeAndFlush(String.format("\"%s\" (votes in topic=%s)\n",
-                            entry.getKey(),
-                            entry.getValue().countVotes()
-                    ));
-        }
+        verify(messageService, times(1)).sendMessage(ctx, response.toString());
         verify(topicRepository, times(1)).findAll();
-        verifyNoMoreInteractions(ctx, topicRepository);
-    }
-
-    @Test
-    void execute_ShowNothing_WhenNoParamsAndTopicsNotExist() {
-        String[] param = new String[]{};
-        when(topicRepository.findAll()).thenReturn(Map.of());
-
-        viewUserCommand.execute(ctx, param);
-
-        verify(topicRepository, times(1)).findAll();
-        verifyNoMoreInteractions(topicRepository);
+        verifyNoMoreInteractions(messageService, topicRepository);
         verifyNoInteractions(ctx);
     }
 
     @Test
-    void execute_SendErrorMessage_WhenSingleParamInvalid() {
-        String[] params = new String[]{"-invalid=Name"};
+    void execute_SendErrorMessage_WhenNoParamsAndTopicsNotExist() {
+        String[] param = new String[]{};
 
-        viewUserCommand.execute(ctx, params);
+        when(topicRepository.findAll()).thenReturn(Map.of());
 
-        verify(ctx, times(1))
-                .writeAndFlush(MessageProviderUtil.getMessage("error.command.view.single_param.invalid"));
-        verifyNoInteractions(topicRepository);
-        verifyNoMoreInteractions(ctx);
+        viewUserCommand.execute(ctx, param);
+
+        verify(messageService, times(1))
+                .sendMessageByKey(ctx, "view.topics.not_found");
+        verify(topicRepository, times(1)).findAll();
+        verifyNoMoreInteractions(topicRepository, messageService);
+        verifyNoInteractions(ctx);
     }
 
     @Test
-    void execute_SendErrorMessage_WhenTopicNameEmpty() {
-        String[] params = new String[]{"-t=  "};
-
-        viewUserCommand.execute(ctx, params);
-
-        verify(ctx, times(1))
-                .writeAndFlush(MessageProviderUtil.getMessage("error.topic.name.empty"));
-        verifyNoInteractions(topicRepository);
-        verifyNoMoreInteractions(ctx);
-    }
-
-    @Test
-    void execute_SendErrorMessage_WhenTopicNotFound() {
+    void execute_SendErrorMessage_WhenSingleParamAndTopicNotFound() {
         String topicName = "TopicName";
         String[] params = new String[]{"-t=" + topicName};
+
         when(topicRepository.findTopicByName(topicName)).thenReturn(Optional.empty());
 
         viewUserCommand.execute(ctx, params);
 
-        verify(ctx, times(1))
-                .writeAndFlush(MessageProviderUtil.getMessage("error.topic.not_found", topicName));
+        verify(messageService, times(1))
+                .sendMessageByKey(ctx, "error.topic.not_found", topicName);
         verify(topicRepository, times(1)).findTopicByName(topicName);
         verifyNoMoreInteractions(topicRepository, ctx);
+        verifyNoInteractions(ctx);
     }
 
     @Test
-    void execute_ShowTopicDetails_WhenTopicExists() {
+    void execute_ShowTopicDetails_WhenSingleParamAndTopicExists() {
         String topicName = "TopicName";
         Topic topic = new Topic(topicName);
         String[] params = new String[]{"-t=" + topicName};
+
         when(topicRepository.findTopicByName(topicName)).thenReturn(Optional.of(topic));
 
         viewUserCommand.execute(ctx, params);
 
-        verify(ctx, times(1)).writeAndFlush(topic.toString());
+        verify(messageService, times(1)).sendMessage(ctx, topic.toString());
         verify(topicRepository, times(1)).findTopicByName(topicName);
-        verifyNoMoreInteractions(topicRepository, ctx);
+        verifyNoMoreInteractions(topicRepository, messageService);
+        verifyNoInteractions(ctx);
     }
 
     @Test
-    void execute_SendErrorMessage_WhenTwoParamsInvalid() {
-        String[] params = new String[]{"-t=Topic", "-invalid=Vote"};
-
-        viewUserCommand.execute(ctx, params);
-
-        verify(ctx, times(1))
-                .writeAndFlush(MessageProviderUtil.getMessage("error.command.view.two_params.invalid"));
-        verifyNoInteractions(topicRepository);
-        verifyNoMoreInteractions(ctx);
-    }
-
-    @Test
-    void execute_SendErrorMessage_WhenTopicOrVoteNameEmpty() {
-        String[] params = new String[]{"-t=Topic", "-v=  "};
-
-        viewUserCommand.execute(ctx, params);
-
-        verify(ctx, times(1))
-                .writeAndFlush(MessageProviderUtil.getMessage("error.topic.name.vote.name.empty"));
-        verifyNoInteractions(topicRepository);
-        verifyNoMoreInteractions(ctx);
-    }
-
-    @Test
-    void execute_SendErrorMessage_WhenTwoParamsValidAndTopicNotFound() {
+    void execute_SendErrorMessage_WhenTwoParamsAndTopicNotFound() {
         String topicName = "TopicName";
         String voteName = "VoteName";
         String[] params = new String[]{"-t=" + topicName, "-v=" + voteName};
+
         when(topicRepository.findTopicByName(topicName)).thenReturn(Optional.empty());
 
         viewUserCommand.execute(ctx, params);
 
-        verify(ctx, times(1))
-                .writeAndFlush(MessageProviderUtil.getMessage("error.topic.not_found", topicName));
+        verify(messageService, times(1))
+                .sendMessageByKey(ctx, "error.topic.not_found", topicName);
         verify(topicRepository, times(1)).findTopicByName(topicName);
-        verifyNoMoreInteractions(topicRepository, ctx);
+        verifyNoMoreInteractions(topicRepository, messageService);
+        verifyNoInteractions(ctx);
     }
 
     @Test
-    void execute_SendErrorMessage_WhenTwoParamsValidAndVoteNotFound() {
+    void execute_SendErrorMessage_WhenTwoParamsAndVoteNotFound() {
         String topicName = "TopicName";
         String voteName = "VoteName";
         Topic topic = mock(Topic.class);
         String[] params = new String[]{"-t=" + topicName, "-v=" + voteName};
+
         when(topicRepository.findTopicByName(topicName)).thenReturn(Optional.of(topic));
         when(topic.getVoteByName(voteName)).thenReturn(Optional.empty());
 
         viewUserCommand.execute(ctx, params);
 
-        verify(ctx, times(1))
-                .writeAndFlush(MessageProviderUtil.getMessage("error.vote.not_found", voteName));
+        verify(messageService, times(1))
+                .sendMessageByKey(ctx, "error.vote.not_found", voteName);
         verify(topicRepository, times(1)).findTopicByName(topicName);
         verify(topic, times(1)).getVoteByName(voteName);
-        verifyNoMoreInteractions(topicRepository, topic, ctx);
+        verifyNoMoreInteractions(topicRepository, topic, messageService);
+        verifyNoInteractions(ctx);
     }
 
     @Test
-    void execute_ShowVoteDetails_WhenTwoParamsValidAndTopicAndVoteExist() {
+    void execute_SendVoteDetails_WhenTwoParamsAndTopicAndVoteExist() {
         String topicName = "TopicName";
         String voteName = "VoteName";
         Topic topic = mock(Topic.class);
         Vote vote = mock(Vote.class);
         String[] params = new String[]{"-t=" + topicName, "-v=" + voteName};
+
         when(topicRepository.findTopicByName(topicName)).thenReturn(Optional.of(topic));
         when(topic.getVoteByName(voteName)).thenReturn(Optional.of(vote));
 
         viewUserCommand.execute(ctx, params);
 
-        verify(ctx, times(1)).writeAndFlush(vote.toString());
+        verify(messageService, times(1)).sendMessage(ctx, vote.toString());
         verify(topicRepository, times(1)).findTopicByName(topicName);
         verify(topic, times(1)).getVoteByName(voteName);
-        verifyNoMoreInteractions(topicRepository, topic, vote);
+        verifyNoMoreInteractions(topicRepository, topic, vote, messageService);
+        verifyNoInteractions(ctx);
     }
 }
